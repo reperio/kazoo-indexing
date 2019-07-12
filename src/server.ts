41e7ed7f2f92b7dcc25b94e716551729604015d3
Spencer Dangel <spencerdangel@sevenhillstechnology.com>
2019-07-12 14:17:11 -0400

    id:3452 Index cdr into elasticsearch

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-import {config} from './config';
-import {Server} from 'hapi';
-import {AppLogger} from './logger';
+import { config } from './config';
+import { Server } from 'hapi';
+import { AppLogger } from './logger';
 import { CrossbarService } from './crossbarService';
+import { ElasticService } from './elasticService';
 import * as moment from 'moment';
 
 const run = async () => {
@@ -14,6 +15,7 @@ const run = async () => {
         method: 'POST',
         handler: async (request, h) => {
             const crossbarService = new CrossbarService(config.crossbarApi, logger);
+            const elasticService = new ElasticService(config.elasticsearchApi, logger);
 
             if (!crossbarService.accountId) {
                 await crossbarService.authenticate();
@@ -23,12 +25,33 @@ const run = async () => {
             logger.debug('Received new call record');
             logger.debug(JSON.stringify(callRecord));
             
-            const timestamp = moment.unix(callRecord.timestamp - 62167219200);
-            const formattedCdrId = `${timestamp.format('YYYYMM')}-${callRecord.call_id}`;
-            logger.debug(formattedCdrId);
+            try {
+                const timestamp = moment.unix(callRecord.timestamp - 62167219200);
+                const formattedCdrId = `${timestamp.format('YYYYMM')}-${callRecord.call_id}`;
+                logger.debug(formattedCdrId);
 
-            const cdr = await crossbarService.getCdrByAccountAndId(callRecord.account_id, formattedCdrId);
-            logger.debug(JSON.stringify(cdr));
+                const cdr = (await crossbarService.getCdrByAccountAndId(callRecord.account_id, formattedCdrId)).data;
+                logger.debug(JSON.stringify(cdr));
+
+                const index = 'cdrs_' + moment.utc(cdr.datetime).format('YYYYMM');
+                const header = {
+                    update: {
+                        _index: index,
+                        _type: '_doc',
+                        _id: cdr.id
+                    }
+                };
+
+                const doc = {
+                    doc: cdr,
+                    doc_as_upsert: true
+                };
+
+                await elasticService.bulkInsert([header, doc]);
+                logger.info(`${formattedCdrId} indexed into elasticsearch`);
+            } catch (err) {
+                logger.error(err);
+            }
 
             return 'success';
         }
